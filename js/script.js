063e306face468277e98dd4612f95b94bffdea8b
Marcin Kaleta <kalet1984@gmail.com>
2019-07-01 15:17:08 +0200

    Done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+var url = 'https://restcountries.eu/rest/v2/name/'; // if not working change v2 back to v1
+var countriesList = document.getElementById('countries');
+
+var search = document.getElementById('search'); // search a country button
+var searchInput = document.getElementById('country-name'); // serach input text area
+
+search.addEventListener('click', searchCountries); // after click search button start searchCountries function
+
+function searchCountries() {
+    var countryName = searchInput.value;
+    if(!countryName.length) countryName = 'Poland'; // if text are is empty set Poland by default
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+    countriesList.innerHTML = '';
+    resp.forEach(function(item) {
+        var liEl = document.createElement('li');
+        liEl.innerText = 'Searched country: '
+        liEl.innerText += item.name +
+        ', Capital name: ' + item.capital;
+        countriesList.appendChild(liEl);
+    });
+  }
